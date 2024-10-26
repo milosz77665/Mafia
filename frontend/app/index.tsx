e559397c864bc0842376e080a05b738c29441b73
Miłosz Marchewka <milosz77665@gmail.com>
2024-10-26 20:04:28 +0200

    add index screen

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from 'react-native';
+import HatIcon from '@/assets/icons/HatIcon';
+import Button from '@/components/CustomButton';
+import CustomText from '@/components/CustomText';
+import { StyleSheet, View } from 'react-native';
 
-export default function Index() {
+const style = StyleSheet.create({
+  indexContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    flex: 1,
+  },
+
+  header: {
+    fontSize: 50,
+  },
+
+  buttonsContainer: {
+    marginTop: 175,
+  },
+
+  joinGameButton: {
+    marginTop: 40,
+  },
+});
+
+const Index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={style.indexContainer}>
+      <HatIcon width={170} height={80} />
+      <CustomText style={style.header}>Mafia</CustomText>
+      <View style={style.buttonsContainer}>
+        <Button onPress={() => {}}>Host Game</Button>
+        <Button onPress={() => {}} buttonStyle={style.joinGameButton}>
+          Join Game
+        </Button>
+      </View>
     </View>
   );
-}
+};
+
+export default Index;
