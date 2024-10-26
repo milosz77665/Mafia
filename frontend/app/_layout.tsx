83e20daab220f1eac6c8a768feb3b352e766ea14
Miłosz Marchewka <milosz77665@gmail.com>
2024-10-26 19:17:04 +0200

    add Lora font

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,41 @@
-import { Stack } from "expo-router";
+import { colors } from '@/constants/colors';
+import { Slot } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
-export default function RootLayout() {
+SplashScreen.preventAutoHideAsync();
+
+const style = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: colors.white,
+  },
+});
+
+const RootLayout = () => {
+  const [loaded, error] = useFonts({
+    'Lora': require('../assets/fonts/Lora-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <View style={style.mainContainer}>
+      <Slot />
+    </View>
   );
-}
+};
+
+export default RootLayout;
