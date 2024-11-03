82c23aa235759ccab63cb0dfed103a5bd3a63d47
Miłosz Marchewka <milosz77665@gmail.com>
2024-11-03 17:26:24 +0100

    refactor code in host screen

--- frontend/app/(home)/host.tsx
+++ frontend/app/(home)/host.tsx
@@ -1,12 +1,12 @@
 import CustomButton from '@/components/CustomButton';
 import CustomText from '@/components/CustomText';
 import CustomInput from '@/components/CustomInput';
-import { FC, useState } from 'react';
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { router } from 'expo-router';
 import CustomSlider from '@/components/CustomSlider';
 import { colors } from '@/constants/colors';
-interface HostProps {}
+
 const style = StyleSheet.create({
   hostContainer: {
     display: 'flex',
@@ -19,69 +19,89 @@ const style = StyleSheet.create({
   titleText: {
     fontSize: 50,
   },
-  input: {},
-  slider: { marginTop: 0, width: 220, height: 20, transform: [{ scaleY: 1.2 }] },
-  gamenameView: {
+
+  label: {
+    fontSize: 20,
+    fontFamily: 'Arial',
+  },
+
+  gameNameContainer: {
     marginTop: 20,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'flex-start',
     flexDirection: 'column',
-    marginBottom: 20,
   },
-  backView: {
-    marginBottom: 50,
+
+  input: {
+    marginTop: 10,
+  },
+
+  lobbySizeContainer: {
+    marginTop: 20,
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-start',
     flexDirection: 'column',
-    flex: 1,
   },
-  label: {
-    marginBottom: 10,
-    fontSize: 20,
-    fontFamily: 'Arial',
-  },
-  sliderLabel: {
+
+  sliderCurrentNumber: {
+    marginTop: 10,
     fontSize: 18,
     fontWeight: 'bold',
     fontFamily: 'Arial',
   },
-  backButton: {},
-  civdiv: { flexDirection: 'row', marginTop: 50 },
-  citLabel: { fontSize: 20, fontFamily: 'Arial', marginRight: 10 },
-  mr30: { marginRight: 30 },
+
+  slider: {
+    width: 220,
+    height: 20,
+    transform: [{ scaleY: 1.2 }],
+  },
+
+  ratioInfoContainer: {
+    flexDirection: 'row',
+    marginTop: 50,
+  },
+
+  citizensNumber: {
+    marginRight: 30,
+  },
+
+  buttonsContainer: {
+    marginTop: 175,
+  },
 });
 
-const Host: FC<HostProps> = () => {
-  const [text, setText] = useState('');
-  const [isFocused, setIsFocused] = useState(false);
-  const changeBorderColor = (isFocused: boolean): string => {
-    return isFocused ? colors.black : 'red'; // Ustawienia kolorów dla focus i blur
-  };
-  const [sliderValue, setSliderValue] = useState(10);
+const Host = () => {
+  const [text, setText] = useState<string>('');
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [sliderValue, setSliderValue] = useState<number>(10);
+
   const mafiaCount = (sliderValue: number): number => {
     if (sliderValue <= 6) return 1;
     if (sliderValue > 6 && sliderValue <= 10) return 2;
     if (sliderValue > 10 && sliderValue <= 14) return 3;
-
     return 4;
   };
+
   const citizenCount = (sliderValue: number): number => {
     if (sliderValue <= 6) return sliderValue - 1;
     if (sliderValue > 6 && sliderValue <= 10) return sliderValue - 2;
     if (sliderValue > 10 && sliderValue <= 14) return sliderValue - 3;
     return sliderValue - 4;
   };
+
   return (
     <View style={style.hostContainer}>
       <View>
         <CustomText style={style.titleText}>Host Game</CustomText>
       </View>
-      <View style={[style.gamenameView]}>
+
+      <View style={style.gameNameContainer}>
         <CustomText style={style.label}> Game name</CustomText>
         <CustomInput
-          inputStyle={[style.input, { borderColor: changeBorderColor(isFocused) }]}
+          containerStyle={style.input}
+          inputStyle={{ borderColor: isFocused ? colors.black : 'red' }}
           value={text}
           onChangeText={setText}
           onFocus={() => setIsFocused(true)}
@@ -89,32 +109,34 @@ const Host: FC<HostProps> = () => {
         />
       </View>
 
-      <CustomText style={style.label}> Set lobby size</CustomText>
-      <CustomText style={style.sliderLabel}>{sliderValue}</CustomText>
-      <CustomSlider
-        sliderStyle={style.slider}
-        value={10}
-        onValueChange={(value) => {
-          setSliderValue(value);
-        }}
-        minimumValue={5}
-        maximumValue={20}
-        step={1}
-        minimumTrackTintColor={colors.black}
-        maximumTrackTintColor={colors.lightGrey}
-        thumbTintColor={colors.grey}
-      />
-      <View style={style.civdiv}>
-        <CustomText style={style.citLabel}> Citizen</CustomText>
-        <CustomText style={style.citLabel}> {citizenCount(sliderValue)}</CustomText>
-        <View style={style.mr30}></View>
-
-        <CustomText style={style.citLabel}> Mafia</CustomText>
-        <CustomText style={style.citLabel}> {mafiaCount(sliderValue)}</CustomText>
+      <View style={style.lobbySizeContainer}>
+        <CustomText style={style.label}> Set lobby size</CustomText>
+        <CustomText style={style.sliderCurrentNumber}>{sliderValue}</CustomText>
+        <CustomSlider
+          sliderStyle={style.slider}
+          value={sliderValue}
+          onValueChange={(value) => {
+            setSliderValue(value);
+          }}
+          minimumValue={5}
+          maximumValue={20}
+          step={1}
+          minimumTrackTintColor={colors.black}
+          maximumTrackTintColor={colors.lightGrey}
+          thumbTintColor={colors.grey}
+        />
       </View>
-      <View style={style.backView}>
+
+      <View style={style.ratioInfoContainer}>
+        <CustomText style={style.label}>Citizens: </CustomText>
+        <CustomText style={[style.label, style.citizensNumber]}>{citizenCount(sliderValue)}</CustomText>
+
+        <CustomText style={style.label}>Mafia: </CustomText>
+        <CustomText style={style.label}>{mafiaCount(sliderValue)}</CustomText>
+      </View>
+
+      <View style={style.buttonsContainer}>
         <CustomButton
-          buttonStyle={style.backButton}
           onPress={() => {
             router.replace('/');
           }}
