ba04b2592d73d72f019392b75965418a78c649b6
Miłosz Marchewka <milosz77665@gmail.com>
2025-01-02 18:44:13 +0100

    add user api

---
+++ frontend/api/api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 5000,
+});
+
+export default api;
