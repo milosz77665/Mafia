8bd7e8b2c66fbc8e97926de3c0ed94bb18919799
Miłosz Marchewka <milosz77665@gmail.com>
2025-01-09 20:05:10 +0100

    add socketApi

---
+++ frontend/api/socketApi.ts
@@ -0,0 +1,90 @@
+import { getFromStorage } from '@/storage/storageHandler';
+import { Manager, Socket } from 'socket.io-client';
+
+class SocketApi {
+  private static instance: SocketApi;
+  private socket: Socket | null = null;
+  private url: string | undefined;
+
+  private constructor(url: string | undefined) {
+    this.url = url;
+  }
+
+  public static getInstance(url: string | undefined) {
+    if (!SocketApi.instance) {
+      SocketApi.instance = new SocketApi(url);
+    }
+    return SocketApi.instance;
+  }
+
+  public connect() {
+    if (!this.socket) {
+      const manager = new Manager(this.url, {
+        transports: ['websocket'],
+        reconnection: true,
+        reconnectionAttempts: 7,
+        reconnectionDelay: 1000,
+        reconnectionDelayMax: 2400,
+      });
+
+      this.socket = manager.socket('/');
+    }
+
+    this.socket.on('connect', () => {
+      console.log('Socket connected:', this.socket?.id);
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('Socket disconnected');
+    });
+
+    this.socket.io.on('reconnect', async () => {
+      const id = await getFromStorage('id');
+      console.log('Reconnecting');
+      this.socket?.emit('reconnect', { id });
+    });
+
+    this.socket.io.on('reconnect_failed', async () => {
+      console.log('Reconnecting failed');
+      this.disconnect();
+    });
+  }
+
+  public disconnect() {
+    if (this.socket) {
+      this.socket.disconnect();
+      this.socket = null;
+    }
+  }
+
+  public emit(event: string, data: any, callback?: (response: any) => void) {
+    if (this.socket) {
+      this.socket.emit(event, data, (response: any) => {
+        if (callback) {
+          callback(response);
+        }
+      });
+    } else {
+      console.error('Socket is not connected');
+    }
+  }
+
+  public on(event: string, callback: (data: any) => void) {
+    if (this.socket) {
+      this.socket.on(event, callback);
+    } else {
+      console.error('Socket is not connected');
+    }
+  }
+
+  public off(event: string, callback?: (data: any) => void) {
+    if (this.socket) {
+      this.socket.off(event, callback);
+    } else {
+      console.error('Socket is not connected');
+    }
+  }
+}
+
+const socketApi = SocketApi.getInstance(process.env.EXPO_PUBLIC_API_URL);
+export default socketApi;
