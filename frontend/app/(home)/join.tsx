35fb6e20de5891aa1e5b7005f7031740af02e80a
Miłosz Marchewka <milosz77665@gmail.com>
2025-01-09 23:21:12 +0100

    implement handleJoin function in join component

--- frontend/app/(home)/join.tsx
+++ frontend/app/(home)/join.tsx
@@ -6,6 +6,12 @@ import { StyleSheet, View } from 'react-native';
 import { useState } from 'react';
 import { useNicknameHandler } from '@/hooks/useNicknameHandler';
 import { useUserDataManager } from '@/hooks/useUserDataManager';
+import socketApi from '@/api/socketApi';
+import { joinLobby, lobbyResponse } from '@/api/lobbyApi';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import { errorActions } from '@/redux/reducers/errorReducer';
+import { gameActions } from '@/redux/reducers/gameReducer';
 
 const style = StyleSheet.create({
   joinContainer: {
@@ -79,13 +85,35 @@ const style = StyleSheet.create({
 });
 
 const Join = () => {
+  const id = useSelector((state: RootState) => state.user.id);
+  const dispatch = useDispatch();
   const { nickname, handleNicknameChange } = useNicknameHandler();
   const { manageUserData } = useUserDataManager();
   const [gameId, setGameId] = useState<string>('');
 
   const handleJoin = async () => {
-    await manageUserData(nickname);
-    router.replace('/lobby');
+    const success = await manageUserData(nickname);
+    if (!success) return;
+    try {
+      socketApi.connect();
+      const data = await joinLobby(id, gameId);
+
+      if (data.lobby) {
+        dispatch(gameActions.setLobby(data.lobby));
+        // dispatch(userActions.setGameId(data.lobby.roomId));
+      }
+
+      router.replace('/lobby');
+    } catch (error) {
+      const response = error as lobbyResponse;
+      dispatch(
+        errorActions.showError({
+          id: Date.now().toString(),
+          title: `Error:`,
+          message: response.message || 'Unexpected error occurred',
+        })
+      );
+    }
   };
 
   return (
